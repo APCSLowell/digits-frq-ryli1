86f69e42bd65476cb772a4392dd0dcb33a4b6d77
ryli1 <ryan792654@gmail.com>
2025-04-09 08:55:45 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,23 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{
+		int n = num;
+		digitList.add(n % 10);
+		while(n > 0) {
+			digitList.set(0, n % 10);
+			n = n / 10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		for(int i = 1; i < digitList.size(); i++) {
+			if(digitList.get(i) < digitList.get(i-1)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public String toString()
